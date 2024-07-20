a2a42c51360a48ed459000dba8ac3b655a8295d2
Ryan Adrian <NayrAdrian24@gmail.com>
2024-07-20 14:40:16 +0800

    Updated the Questions, It is now displayed on the canvas.

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 
 THEME_COLOR = "#375362"
@@ -6,7 +7,9 @@ THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -18,7 +21,7 @@ class QuizInterface:
         # Create a canvas and place it below the score label
         self.canvas = Canvas(self.window, height=250, width=300, bg="white", highlightthickness=0)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=30)
-        self.question = self.canvas.create_text(150, 125, text="Sample", font=("Arial", 15, "italic"))
+        self.question_text = self.canvas.create_text(150, 125, text="Sample", width=280, font=("Arial", 15, "italic"))
 
         # True/False Buttons
         cross_image = PhotoImage(file="images/false.png")
@@ -29,4 +32,10 @@ class QuizInterface:
         self.true_button = Button(image=check_image, highlightthickness=0, bg=THEME_COLOR)
         self.true_button.grid(row=2, column=0)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
